c630327d4e1fd9a551db17631717804aecc20094
Edward Nolan <nolanee@umich.edu>
2020-05-08 11:37:38 -0400

    finished FindIndex

--- main.go
+++ main.go
@@ -8,19 +8,38 @@ import (
 func main() {
 	fmt.Println("hello!")
 	arr := [8]int{}
-	i := FindIndex(arr, Identity, 0)
+	pred := predicator(0)
+	pred2 := predicator(1)
+	i := FindIndex(arr, pred, 0)
+	fmt.Println(i)
+	i = FindIndex(arr, pred2, 0)
+	fmt.Println(i)
+	arr[0] = 1
+	i = FindIndex(arr, pred2, 2)
+	fmt.Println(i)
+	i = FindIndex(arr, pred2, 0)
+	fmt.Println(i)
+	arr[7] = 1
+	i = FindIndex(arr, pred2, 2)
 	fmt.Println(i)
 
 }
 
-// Identity is the default predicate. It checks whether two values are the same
-func Identity(x interface{}, y interface{}) bool {
-	return x == y
+func predicator(search interface{}) func(interface{}) bool {
+	return func(value interface{}) bool {
+		return search == value
+	}
 }
 
-// FindIndex returns the first index at which predicate is true. TODO add defaault values
-func FindIndex(arr interface{}, predicate func(interface{}, interface{}) bool, fromIndex int) int {
-	t := reflect.TypeOf(arr)
-	fmt.Println(t)
-	return 0
+// FindIndex returns the first index at which predicate is true. Will panic if interface is not an
+// array, slice, or string
+func FindIndex(arrayInterface interface{}, predicate func(interface{}) bool, fromIndex int) int {
+	arr := reflect.ValueOf(arrayInterface)
+	l := arr.Len()
+	for i := fromIndex; i < l; i++ {
+		if predicate(arr.Index(i).Interface()) {
+			return i
+		}
+	}
+	return -1
 }
