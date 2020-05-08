45fe3509f4ec384cd582935fb0660b2c5d6d4a64
Edward Nolan <nolanee@umich.edu>
2020-05-08 12:35:28 -0400

    added Find

--- main.go
+++ main.go
@@ -8,20 +8,29 @@ import (
 func main() {
 	fmt.Println("hello!")
 	arr := [8]int{}
+
 	pred := predicator(0)
-	pred2 := predicator(1)
 	i := FindIndex(arr, pred, 0)
 	fmt.Println(i)
+
+	pred2 := predicator(1)
 	i = FindIndex(arr, pred2, 0)
 	fmt.Println(i)
 	arr[0] = 1
 	i = FindIndex(arr, pred2, 2)
 	fmt.Println(i)
+	arr[7] = 1
 	i = FindIndex(arr, pred2, 0)
 	fmt.Println(i)
-	arr[7] = 1
 	i = FindIndex(arr, pred2, 2)
 	fmt.Println(i)
+
+	pred3 := predicator(7)
+	arr[6] = 7
+	v := Find(arr, pred3, 2).(int)
+	fmt.Println(v)
+	e := Find(arr, pred3, 7)
+	fmt.Println(e)
 
 }
 
@@ -32,7 +41,7 @@ func predicator(search interface{}) func(interface{}) bool {
 }
 
 // FindIndex returns the first index at which predicate is true. Will panic if interface is not an
-// array, slice, or string
+// array, slice, or string. If not found, returns -1.
 func FindIndex(arrayInterface interface{}, predicate func(interface{}) bool, fromIndex int) int {
 	arr := reflect.ValueOf(arrayInterface)
 	l := arr.Len()
@@ -42,4 +51,16 @@ func FindIndex(arrayInterface interface{}, predicate func(interface{}) bool, fro
 		}
 	}
 	return -1
+}
+
+// Find returns the first value at which is true within interface. Will panic if interface is not an
+// array, slice, or string. If not found returns an empty interface.
+func Find(arrayInterface interface{}, predicate func(interface{}) bool, fromIndex int) interface{} {
+	i := FindIndex(arrayInterface, predicate, fromIndex)
+	if i == -1 {
+		var empty interface{}
+		return empty
+	}
+	arrV := reflect.ValueOf(arrayInterface).Index(i).Interface()
+	return arrV
 }
